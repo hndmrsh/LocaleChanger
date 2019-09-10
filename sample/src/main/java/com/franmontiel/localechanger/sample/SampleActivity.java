d40123488a55f6b6b084ba1f9ed004ca966ecc98
Jamie Sanson <jamie.sanson@trademe.co.nz>
2019-09-11 08:58:16 +1200

    Migrates project to AndroidX

--- sample/src/main/java/com/franmontiel/localechanger/sample/SampleActivity.java
+++ sample/src/main/java/com/franmontiel/localechanger/sample/SampleActivity.java
@@ -20,7 +20,7 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
